30521af6863cff520c65c5f3946158a5c05912ef
Lewis <lewisthornton@btinternet.com>
2023-01-14 17:30:58 +0000

    Added .npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export { queryToUrl } from './urlBuilder'
-export * from './types/alphavantage/queries/avQuery.types'
+export * from './types/alphavantage/queries/stockQueries.types'
+export * from './types/alphavantage/queries/alphaIntelQueries.types'
+export * from './types/alphavantage/queries/commoditiesQueries.types'
+export * from './types/alphavantage/queries/cryptoQueries.types'
+export * from './types/alphavantage/queries/forexQueries.types'
+export * from './types/alphavantage/queries/fundamentalsQueries.types'
+export * from './types/alphavantage/queries/econIndQueries.types'
+
+export * from './types/query'
+export * from './types/constants'
+export * from './types/enums'
 
